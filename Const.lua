0c4cfb374ad7a4ff9021c51ceeefc5fb78763a27
nSimon (Nicolas) <contact@nsimon.fr>
2019-12-12 14:14:35 +0100

    Add PvP Items restrictions to Const.lua

--- Const.lua
+++ Const.lua
@@ -68,4 +68,24 @@ Restrictions = {
         inInstanceIds = { 1220, 1669 }, -- Broken Isles, Argus
         inInstanceTypes = { "none" },
     },
+    
+    [19307] = { -- Alterac Heavy Runecloth Bandage
+        inInstanceIds = { 30 }, -- Alterac Valley
+    },
+    [19066] = { -- Warsong Gulch Runecloth Bandage
+        inInstaceIds = { 489 }, -- Warsong Gulch
+    },
+    
+    [17349] = { -- Superior Healing Draught
+        inInstaceIds = { 30, 489 }, -- Alterac Valley, Warsong Gulch
+    },
+    [17352] = { -- Superior Mana Draught
+        inInstaceIds = { 30, 489 }, -- Alterac Valley, Warsong Gulch
+    },
+    [17351] = { -- Major Mana Draught
+        inInstaceIds = { 30, 489 }, -- Alterac Valley, Warsong Gulch
+    },
+    [17348] = { -- Major Healing Draught
+        inInstaceIds = { 30, 489 }, -- Alterac Valley, Warsong Gulch
+    },
 }
